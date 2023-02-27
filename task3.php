f4faab6801c059d298cee1a0e557f50ec672b7a3
Saidul Islam <42485291+saidulislam01@users.noreply.github.com>
2023-02-27 21:21:03 +0600

    Task 3

---
+++ task3.php
@@ -0,0 +1,29 @@
+<?php
+class Person {
+    private $name;
+    private $email;
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+}
+
+$person = new Person();
+$person->setName($_POST['name']);
+$person->setEmail($_POST['email']);
+echo $person->getName();
+echo $person->getEmail();
+
+?>
